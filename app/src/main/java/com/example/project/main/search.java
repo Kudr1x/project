1f7faf49a2d6f1e43d2bcb7441776d72b5640fe3
kudrix <max51kud@gmail.com>
2023-04-13 02:54:27 +0500

    map in fragment

--- app/src/main/java/com/example/project/main/search.java
+++ app/src/main/java/com/example/project/main/search.java
@@ -1,32 +1,50 @@
 package com.example.project.main;
 
+import android.Manifest;
 import android.app.Dialog;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.graphics.Point;
 import android.graphics.drawable.ColorDrawable;
+import android.location.Location;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Patterns;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.project.R;
 import com.example.project.util.SelectListenerString;
 import com.example.project.util.carsAdapter;
 import com.github.florent37.singledateandtimepicker.dialog.SingleDateAndTimePickerDialog;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.yandex.mapkit.Animation;
+import com.yandex.mapkit.MapKitFactory;
+import com.yandex.mapkit.map.CameraListener;
+import com.yandex.mapkit.map.CameraPosition;
+import com.yandex.mapkit.map.CameraUpdateReason;
+import com.yandex.mapkit.map.Map;
+import com.yandex.mapkit.mapview.MapView;
+
 
 import java.util.ArrayList;
 import java.util.Date;
+
 
 public class search extends Fragment implements SelectListenerString {
 
@@ -35,19 +53,22 @@ public class search extends Fragment implements SelectListenerString {
     Button add;
     Button all;
     Dialog dialog;
+    MapView mapView;
     RecyclerView recyclerView;
     boolean flag = true;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+        MapKitFactory.setApiKey("bdd998b5-582a-4519-ab3f-424df6763579");
+        MapKitFactory.initialize(getContext());
         return inflater.inflate(R.layout.fragment_search, container, false);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        mapView = view.findViewById(R.id.mapview);
 
         user = view.findViewById(R.id.user);
         driver = view.findViewById(R.id.driver);
@@ -115,5 +136,19 @@ public class search extends Fragment implements SelectListenerString {
         arr.add("Приора");
         arr.add("Гелик");
         recyclerView.setAdapter(new carsAdapter(arr, this, getActivity()));
+    }
+
+    @Override
+    public void onStop() {
+        mapView.onStop();
+        MapKitFactory.getInstance().onStop();
+        super.onStop();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        MapKitFactory.getInstance().onStart();
+        mapView.onStart();
     }
 }
